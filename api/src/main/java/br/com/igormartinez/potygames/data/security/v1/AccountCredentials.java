4e6e7c5e673d2b3d022fec26e05e2174c2c64d27
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-08 15:48:43 -0300

    Add objects to Auth methods

---
+++ api/src/main/java/br/com/igormartinez/potygames/data/security/v1/AccountCredentials.java
@@ -0,0 +1,64 @@
+package br.com.igormartinez.potygames.data.security.v1;
+
+import java.io.Serializable;
+
+public class AccountCredentials implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+
+    public AccountCredentials() { }
+    
+    public AccountCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AccountCredentials other = (AccountCredentials) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+}
