93e2fe3cb3125fcabd7fc184aa3aff929adac27a
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-08 19:57:45 -0300

    Add findById, create, update and delete methods in UserController/UserService

--- api/src/main/java/br/com/igormartinez/potygames/controllers/UserController.java
+++ api/src/main/java/br/com/igormartinez/potygames/controllers/UserController.java
@@ -3,10 +3,18 @@ package br.com.igormartinez.potygames.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.igormartinez.potygames.data.dto.v1.UserDTO;
+import br.com.igormartinez.potygames.data.dto.v1.UserRegistrationDTO;
 import br.com.igormartinez.potygames.models.User;
 import br.com.igormartinez.potygames.services.UserService;
 
@@ -18,7 +26,28 @@ public class UserController {
     UserService service;
 
     @GetMapping
-    public List<User> getUsers() {
+    public List<User> findAll() {
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable(value = "id") Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public UserRegistrationDTO create(@RequestBody UserRegistrationDTO userDTO) {
+        return service.create(userDTO);
+    }
+
+    @PutMapping
+    public UserDTO update(@RequestBody UserDTO userDTO) {
+        return service.update(userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- api/src/main/java/br/com/igormartinez/potygames/services/UserService.java
+++ api/src/main/java/br/com/igormartinez/potygames/services/UserService.java
@@ -8,8 +8,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.com.igormartinez.potygames.data.dto.v1.UserDTO;
+import br.com.igormartinez.potygames.data.dto.v1.UserRegistrationDTO;
+import br.com.igormartinez.potygames.exceptions.RequiredObjectIsNullException;
+import br.com.igormartinez.potygames.exceptions.ResourceAlreadyExistsException;
+import br.com.igormartinez.potygames.exceptions.ResourceNotFoundException;
 import br.com.igormartinez.potygames.models.User;
 import br.com.igormartinez.potygames.repositories.UserRepository;
+import br.com.igormartinez.potygames.security.PasswordManager;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,7 +23,10 @@ public class UserService implements UserDetailsService {
     @Autowired
     UserRepository repository;
 
-    public UserService(UserRepository repository) {
+    @Autowired
+    PasswordManager passwordManager;
+
+    public UserService(UserRepository repository, PasswordManager passwordManager) {
         this.repository = repository;
     }
 
@@ -26,12 +35,88 @@ public class UserService implements UserDetailsService {
         User user = repository.findByEmail(email);
         
         if (user == null) 
-            throw new UsernameNotFoundException("User with email " + email + " not found");
+            throw new UsernameNotFoundException("User not found");
 
         return user;
     }
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(Long id){
+        return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    public UserRegistrationDTO create(UserRegistrationDTO userDTO) {
+        if (userDTO == null) 
+            throw new RequiredObjectIsNullException("Request object cannot be null");
+
+        if (repository.findByEmail(userDTO.getEmail()) != null)
+            throw new ResourceAlreadyExistsException("User alrealdy exists");
+
+        User user = new User();
+        user.setEmail(userDTO.getEmail());
+        user.setName(userDTO.getName());
+        user.setPassword(passwordManager.encodePassword(userDTO.getPassword()));
+        user.setAccountNonExpired(Boolean.TRUE);
+        user.setAccountNonLocked(Boolean.TRUE);
+        user.setCredentialsNonExpired(Boolean.TRUE);
+        user.setEnabled(Boolean.TRUE);
+
+        User createdUser = repository.save(user);
+
+        return new UserRegistrationDTO(createdUser.getName(), createdUser.getEmail(), "");
+    }
+
+    public UserDTO update(UserDTO userDTO) {
+        if (userDTO == null 
+                || userDTO.getId() == null || userDTO.getId() <= 0
+                || userDTO.getName() == null || userDTO.getName().isBlank() 
+                || userDTO.getEmail() == null || userDTO.getEmail().isBlank()
+                || userDTO.getPassword() == null || userDTO.getPassword().isBlank()
+                || userDTO.getAccountNonExpired() == null || userDTO.getAccountNonLocked() == null
+                || userDTO.getCredentialsNonExpired() == null || userDTO.getEnabled() == null) 
+            throw new RequiredObjectIsNullException("Request object cannot be null");
+
+        if (!repository.existsById(userDTO.getId())) 
+            throw new ResourceNotFoundException("User not found");
+        
+        User user = repository.findByEmail(userDTO.getEmail());
+        if (user != null && user.getId() != userDTO.getId())
+            throw new ResourceAlreadyExistsException("User alrealdy exists");
+
+        if (user == null)
+            user = new User();
+
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordManager.encodePassword(userDTO.getPassword()));
+        user.setAccountNonExpired(userDTO.getAccountNonExpired());
+        user.setAccountNonLocked(userDTO.getAccountNonLocked());
+        user.setCredentialsNonExpired(userDTO.getCredentialsNonExpired());
+        user.setEnabled(userDTO.getCredentialsNonExpired());
+
+        User updatedUser = repository.save(user);
+
+        UserDTO updatedUserDTO = new UserDTO();
+        updatedUserDTO.setId(updatedUser.getId());
+        updatedUserDTO.setName(updatedUser.getName());
+        updatedUserDTO.setEmail(updatedUser.getEmail());
+        updatedUserDTO.setPassword("");
+        updatedUserDTO.setAccountNonExpired(updatedUser.getAccountNonExpired());
+        updatedUserDTO.setAccountNonLocked(updatedUser.getAccountNonLocked());
+        updatedUserDTO.setCredentialsNonExpired(updatedUser.getCredentialsNonExpired());
+        updatedUserDTO.setEnabled(updatedUser.getEnabled());
+
+        return updatedUserDTO;
+    }
+
+    public void delete(Long id) {
+        if (!repository.existsById(id))
+            throw new ResourceNotFoundException("User not found");
+        
+        repository.deleteById(id);
     }
 }
