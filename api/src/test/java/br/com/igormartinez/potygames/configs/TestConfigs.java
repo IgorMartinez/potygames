dc37e4bc47f60d61c8063a38bba00fce1318b4db
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-09 11:06:37 -0300

    Add configuration to tests enviroment

---
+++ api/src/test/java/br/com/igormartinez/potygames/configs/TestConfigs.java
@@ -0,0 +1,13 @@
+package br.com.igormartinez.potygames.configs;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+
+    public static final String CONTENT_TYPE_JSON = "application/json";
+
+    public static final String ORIGIN_LOCALHOST = "http://localhost:8080";
+    public static final String ORIGIN_NOTALLOWED = "https://www.google.com/";
+}
