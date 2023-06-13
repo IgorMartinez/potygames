12d1efa7879af7638f05f3a6e286c39b057e2668
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-13 19:45:28 -0300

    Add OpenAPI documentation and change returns

--- api/src/main/java/br/com/igormartinez/potygames/controllers/AuthController.java
+++ api/src/main/java/br/com/igormartinez/potygames/controllers/AuthController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.igormartinez.potygames.data.security.v1.AccountCredentials;
+import br.com.igormartinez.potygames.data.security.v1.Token;
 import br.com.igormartinez.potygames.services.AuthService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,12 +24,27 @@ public class AuthController {
     @Autowired
     AuthService service;
 
+    @Operation(
+        summary = "Authenticates a user and return a token",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PostMapping("/signin")
-    @SuppressWarnings("rawtypes")
-    public ResponseEntity signin(@RequestBody AccountCredentials accountCredentials) {
-        return ResponseEntity.ok(service.signin(accountCredentials));
+    public Token signin(@RequestBody AccountCredentials accountCredentials) {
+        return service.signin(accountCredentials);
     }
 
+    @Operation(
+        summary = "Refresh token for authenticated user and returns a token",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PutMapping("/refresh")
     @SuppressWarnings("rawtypes")
     public ResponseEntity refresh(@RequestHeader("Authorization") String refreshToken) {
