3809488953a040bbefcfb780d8bee28c99147425
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-14 20:13:10 -0300

    Add script to drop all tables in database

---
+++ db/drop_tables.sql
@@ -0,0 +1,7 @@
+-- Drop all tables of Poty Games Database
+DROP TABLE flyway_schema_history;
+DROP TABLE user_addresses;
+DROP TABLE user_contacts;
+DROP TABLE user_permissions;
+DROP TABLE users;
+DROP TABLE permissions;
