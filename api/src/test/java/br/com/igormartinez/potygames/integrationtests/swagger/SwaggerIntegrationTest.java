9892f2dcd6c1e5abd03fe0cac4d9c2ced52626f9
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-14 10:56:53 -0300

    Add swagger integration test

---
+++ api/src/test/java/br/com/igormartinez/potygames/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,32 @@
+package br.com.igormartinez.potygames.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.igormartinez.potygames.configs.TestConfigs;
+import br.com.igormartinez.potygames.integrationtests.testcontainers.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+	@Test
+	void shouldDisplaySwaggerUiPage() {
+		String content = 
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
