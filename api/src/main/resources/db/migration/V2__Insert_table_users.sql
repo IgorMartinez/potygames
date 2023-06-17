de9acff19b9200d232ada0b535738197188030b3
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-17 10:41:13 -0300

    Add phoneNumber in table users

--- api/src/main/resources/db/migration/V2__Insert_table_users.sql
+++ api/src/main/resources/db/migration/V2__Insert_table_users.sql
@@ -1,14 +1,14 @@
 -- Data generated via Mockaroo
 
-INSERT INTO users (email, password, name, birth_date, document_number, account_non_expired, account_non_locked, credentials_non_expired, enabled)
+INSERT INTO users (email, password, name, birth_date, document_number, phone_number, account_non_expired, account_non_locked, credentials_non_expired, enabled)
 VALUES 
-('rlayzell0@pen.io', '7a69cee5f04bfbf66ca7974d41ff872e6d84ee83f2c379605a7fd5bba5ac3df9b5b113bb2ad5bb2c', 'Rey Layzell', '1966-06-11', '755.507.583-29', true, true, true, true),
-('fragge1@blinklist.com', '6c8fd93db6f0f35fb60b169d2bec3636b76b18143e10a5bae1b51dd0065d32482cd3ee189a796790', 'Fayre Ragge', '1984-04-24', '917.590.242-42', true, true, true, true),
-('ecomberbeach2@forbes.com', '185b41579aba0fc1d84dba99c7179bb256e2aca5edfef6cbb859534ed2b2b83ce1c8d88229746586', 'Emelen Comberbeach', '1972-05-30', '154.032.675-44', true, true, true, false),
-('astanlike3@xinhuanet.com', 'cbb7931e97329a6186158613dff57403bec825c8574d256113736bfa8b98b4e32f725a175b0207d3', 'Anjanette Stanlike', '1983-12-15', '999.963.723-77', true, true, false, false),
-('vhorbart4@hexun.com', 'ee1ec6844724c68fb80a8fa0c78572f26ff854e1b0612a5880eedf1047366f7621c1ba3d2c5f40ff', 'Vallie Horbart', '2000-09-04', '103.467.163-55', false, false, true, false),
-('mzanini5@si.edu', '52dcaf366e10687041461aa2e9ef0c2dbfbbdb543c99ec78b1be1b6c9457ab557b08c85463c18f7d', 'Maurizio Zanini', '1968-08-21', '733.640.172-59', false, false, false, true),
-('tcamelia6@dot.gov', '81836b8319d936001d58c7baddb30fa9aeadc56341bae06886c7061ce76cac00aa82d767ac0a83b1', 'Travers Camelia', '1972-09-18', '147.866.543-95', false, false, true, false),
-('fdonat7@whitehouse.gov', '804ad4bb0f3a2ec9d9effb74d7462a32932b3b2cb176517c59dcee18af3ca2a458027376ae3f0c6f', 'Fallon Donat', '1987-10-18', '708.811.938-09', true, true, false, true),
-('cchadderton8@shinystat.com', 'faad75b41e5b6c634c86bcb06739668227564ca5a686d349409fff44bc5e3364cec4fa474dbf8612', 'Cloris Chadderton', '1999-07-28', '368.199.028-15', false, false, false, false),
-('cminett9@msu.edu', '371f291aa8d1ed9c091ae63a1a062946510ed9e7dbbccc0da7d83db8186b5d2dba3cb1570f85274f', 'Celle Minett', '1988-08-15', '619.362.869-65', true, true, false, false);
+('rlayzell0@pen.io', '7a69cee5f04bfbf66ca7974d41ff872e6d84ee83f2c379605a7fd5bba5ac3df9b5b113bb2ad5bb2c', 'Rey Layzell', '1966-06-11', '755.507.583-29', '+5545940815815', true, true, true, true),
+('fragge1@blinklist.com', '6c8fd93db6f0f35fb60b169d2bec3636b76b18143e10a5bae1b51dd0065d32482cd3ee189a796790', 'Fayre Ragge', '1984-04-24', '917.590.242-42', '+5500905884939', true, true, true, true),
+('ecomberbeach2@forbes.com', '185b41579aba0fc1d84dba99c7179bb256e2aca5edfef6cbb859534ed2b2b83ce1c8d88229746586', 'Emelen Comberbeach', '1972-05-30', '154.032.675-44', '+5581955294821', true, true, true, false),
+('astanlike3@xinhuanet.com', 'cbb7931e97329a6186158613dff57403bec825c8574d256113736bfa8b98b4e32f725a175b0207d3', 'Anjanette Stanlike', '1983-12-15', '999.963.723-77', '+5547947907831', true, true, false, false),
+('vhorbart4@hexun.com', 'ee1ec6844724c68fb80a8fa0c78572f26ff854e1b0612a5880eedf1047366f7621c1ba3d2c5f40ff', 'Vallie Horbart', '2000-09-04', '103.467.163-55', '+5565901976027', false, false, true, false),
+('mzanini5@si.edu', '52dcaf366e10687041461aa2e9ef0c2dbfbbdb543c99ec78b1be1b6c9457ab557b08c85463c18f7d', 'Maurizio Zanini', '1968-08-21', '733.640.172-59', '+5546954891210', false, false, false, true),
+('tcamelia6@dot.gov', '81836b8319d936001d58c7baddb30fa9aeadc56341bae06886c7061ce76cac00aa82d767ac0a83b1', 'Travers Camelia', '1972-09-18', '147.866.543-95', '+5573930582515', false, false, true, false),
+('fdonat7@whitehouse.gov', '804ad4bb0f3a2ec9d9effb74d7462a32932b3b2cb176517c59dcee18af3ca2a458027376ae3f0c6f', 'Fallon Donat', '1987-10-18', '708.811.938-09', '+5575983816126', true, true, false, true),
+('cchadderton8@shinystat.com', 'faad75b41e5b6c634c86bcb06739668227564ca5a686d349409fff44bc5e3364cec4fa474dbf8612', 'Cloris Chadderton', '1999-07-28', '368.199.028-15', '+5502986522113', false, false, false, false),
+('cminett9@msu.edu', '371f291aa8d1ed9c091ae63a1a062946510ed9e7dbbccc0da7d83db8186b5d2dba3cb1570f85274f', 'Celle Minett', '1988-08-15', '619.362.869-65', '+5585925463215', true, true, false, false);
--- api/src/main/resources/db/migration/V9__Create_table_user_contacts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_contacts (
-    id SERIAL,
-    id_user INTEGER NOT NULL,
-    favorite BOOLEAN DEFAULT FALSE,
-    description TEXT,
-    phone_number TEXT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE
-);
