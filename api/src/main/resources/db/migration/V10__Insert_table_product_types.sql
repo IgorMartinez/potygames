3e691d1b851e514f4018941fd988b17a5c42dc9c
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-20 19:03:06 -0300

    Add migrations with mock data in product_types and products

---
+++ api/src/main/resources/db/migration/V10__Insert_table_product_types.sql
@@ -0,0 +1,14 @@
+-- Data generated via Mockaroo
+
+INSERT INTO product_types (description)
+VALUES 
+('Convolvulaceae'),
+('Euphorbiaceae'),
+('Opegraphaceae'),
+('Polemoniaceae'),
+('Scrophulariaceae'),
+('Pyrenulaceae'),
+('Violaceae'),
+('Iridaceae'),
+('Oleaceae'),
+('Rosaceae');
