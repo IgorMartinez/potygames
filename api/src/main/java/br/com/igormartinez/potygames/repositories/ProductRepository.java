8fc6034ff4e4920360c502c2ae8f81f9174c36f9
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-21 18:54:22 -0300

    Add new method in ProductRepository to count the all products with the given idProductType

--- api/src/main/java/br/com/igormartinez/potygames/repositories/ProductRepository.java
+++ api/src/main/java/br/com/igormartinez/potygames/repositories/ProductRepository.java
@@ -1,9 +1,14 @@
 package br.com.igormartinez.potygames.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.igormartinez.potygames.models.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+    @Query("SELECT COUNT(p) FROM Product p WHERE p.type.id = :idProductType")
+    int countProductsByIdProductType(long idProductType);
+}
