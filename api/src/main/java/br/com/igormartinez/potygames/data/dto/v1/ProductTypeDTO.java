067c7eeb643a28abf31254d471a3608bf4452286
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-06-21 18:54:48 -0300

    Add ProductType DTO and mapper

---
+++ api/src/main/java/br/com/igormartinez/potygames/data/dto/v1/ProductTypeDTO.java
@@ -0,0 +1,6 @@
+package br.com.igormartinez.potygames.data.dto.v1;
+
+public record ProductTypeDTO(
+    Long id,
+    String description
+) {}
---
+++ api/src/main/java/br/com/igormartinez/potygames/mappers/ProductTypeDTOMapper.java
@@ -0,0 +1,20 @@
+package br.com.igormartinez.potygames.mappers;
+
+import java.util.function.Function;
+
+import org.springframework.stereotype.Service;
+
+import br.com.igormartinez.potygames.data.dto.v1.ProductTypeDTO;
+import br.com.igormartinez.potygames.models.ProductType;
+
+@Service
+public class ProductTypeDTOMapper implements Function<ProductType, ProductTypeDTO> {
+
+    @Override
+    public ProductTypeDTO apply(ProductType type) {
+        return new ProductTypeDTO(
+            type.getId(), 
+            type.getDescription());
+    }
+    
+}
