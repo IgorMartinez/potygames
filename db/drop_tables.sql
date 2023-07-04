c42f389595dfd61709076d9a45fc174298185dcf
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-07-04 14:03:58 -0300

    Add yugioh tables in drop all script

--- db/drop_tables.sql
+++ db/drop_tables.sql
@@ -1,5 +1,11 @@
 -- Drop all tables of Poty Games Database
 DROP TABLE flyway_schema_history;
+
+DROP TABLE yugioh_cards;
+DROP TABLE yugioh_card_types;
+DROP TABLE yugioh_card_categories;
+DROP TYPE YUGIOH_CARD_ATTRIBUTE;
+
 DROP TABLE products;
 DROP TABLE product_types;
 
