bac4f21281b0160b953bd596163e56017b28b86e
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-07-06 12:01:50 -0300

    Add OpenAPI documentation in YugiohCardController

--- api/src/main/java/br/com/igormartinez/potygames/controllers/YugiohCardController.java
+++ api/src/main/java/br/com/igormartinez/potygames/controllers/YugiohCardController.java
@@ -24,6 +24,9 @@ import br.com.igormartinez.potygames.enums.YugiohCardAttribute;
 import br.com.igormartinez.potygames.models.YugiohCardCategory;
 import br.com.igormartinez.potygames.models.YugiohCardType;
 import br.com.igormartinez.potygames.services.YugiohCardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping("/api/v1/product/yugioh-card")
@@ -32,6 +35,13 @@ public class YugiohCardController {
     @Autowired
     YugiohCardService service;
     
+    @Operation(
+        summary = "Find all Yu-Gi-Oh! cards",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @GetMapping
     public Page<YugiohCardDTO> findAll(
         @RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -43,16 +53,47 @@ public class YugiohCardController {
         return service.findAll(pageable);
     }
 
+    @Operation(
+        summary = "Find a Yu-Gi-Oh! card by given id",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @GetMapping("/{yugioh-card-id}")
     public YugiohCardDTO findById(@PathVariable("yugioh-card-id") Long id) {
         return service.findById(id);
     }
 
+    @Operation(
+        summary = "Creates a new Yu-Gi-Oh! card",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PostMapping
     public YugiohCardDTO create(@RequestBody YugiohCardDTO cardDTO) {
         return service.create(cardDTO);
     }
 
+    @Operation(
+        summary = "Update a Yu-Gi-Oh! card",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PutMapping("/{yugioh-card-id}")
     public YugiohCardDTO update(
         @PathVariable("yugioh-card-id") Long id,
@@ -60,22 +101,54 @@ public class YugiohCardController {
         return service.update(id, cardDTO);
     }
 
+    @Operation(
+        summary = "Delete a Yu-Gi-Oh! card",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @DeleteMapping("/{yugioh-card-id}")
     public ResponseEntity<?> delete(@PathVariable("yugioh-card-id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Find all the attributes of a Yu-Gi-Oh! card",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @GetMapping("/attribute")
     public List<YugiohCardAttribute> findAllAttributes() {
         return service.findAllAttributes();
     }
 
+    @Operation(
+        summary = "Find all the categories of a Yu-Gi-Oh! card",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @GetMapping("/category")
     public List<YugiohCardCategory> findAllCategories() {
         return service.findAllCategories();
     }
 
+    @Operation(
+        summary = "Find all the types of a Yu-Gi-Oh! card",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @GetMapping("/type")
     public List<YugiohCardType> findAllTypes() {
         return service.findAllTypes();
