5de1d35f94d81edfe39142bc3b5b80c1e3be5ad6
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-09-08 14:58:05 -0300

    Add inventory_items in drop script

--- db/drop_tables.sql
+++ db/drop_tables.sql
@@ -1,12 +1,7 @@
 -- Drop all tables of Poty Games Database
 DROP TABLE flyway_schema_history;
 
-DROP TABLE yugioh_cards;
-DROP TABLE yugioh_card_types;
-DROP TABLE yugioh_card_categories;
-DROP CAST(varchar AS yugioh_card_attribute);
-DROP TYPE YUGIOH_CARD_ATTRIBUTE;
-
+DROP TABLE inventory_items;
 DROP TABLE products;
 DROP TABLE product_types;
 
