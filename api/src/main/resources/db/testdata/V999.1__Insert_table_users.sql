24a34c701873e3ed9f3e0fb1e3b2a1bd989704c1
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-09-08 11:07:51 -0300

    Split migrations in default and test

---
+++ api/src/main/resources/db/testdata/V999.1__Insert_table_users.sql
@@ -0,0 +1,6 @@
+-- Data generated via Mockaroo
+
+INSERT INTO users (email, password, name, birth_date, document_number, phone_number, account_non_expired, account_non_locked, credentials_non_expired, enabled)
+VALUES 
+('admin@potygames.com', '298d73912421e7b91fc667bb3e1064df407e1e166b930bfc343a85a3f019cfb4434a99e9ea36e196', 'Admin', '1966-06-11', '000.000.000-00', '+5500000000000', true, true, true, true),
+('customer@potygames.com', 'e853eaa35d3f19e90ba8082ee6aaebf97ab1767aa3e847c1ffbe05e0ce2099fb0eae5a0cdfe97e07', 'Customer', '1988-06-11', '111.111.111-11', '+5511111111111', true, true, true, true);
--- api/src/main/resources/db/testdata/V999.2__Insert_table_user_permissions.sql
+++ api/src/main/resources/db/testdata/V999.2__Insert_table_user_permissions.sql
@@ -1,12 +1,5 @@
 INSERT INTO user_permissions (id_user, id_permission)
 VALUES 
 (1,1),
-(2,2),
-(3,1),
-(4,2),
-(5,2),
-(6,2),
-(7,2),
-(8,2),
-(9,2),
-(10,2);
+(1,2),
+(2,2);
