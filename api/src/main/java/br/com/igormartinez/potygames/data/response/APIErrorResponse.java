ea5443698b1197185e1d808b909f931e005cd9ed
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-09-12 16:26:02 -0300

    Adjust APIErrorResponse to not show fields when they are null

--- api/src/main/java/br/com/igormartinez/potygames/data/response/APIErrorResponse.java
+++ api/src/main/java/br/com/igormartinez/potygames/data/response/APIErrorResponse.java
@@ -2,6 +2,10 @@ package br.com.igormartinez.potygames.data.response;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public record APIErrorResponse(
     String type,
     String title,
