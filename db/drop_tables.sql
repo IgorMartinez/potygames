cbbf2504a88956dba9264d0e208087ee12090a41
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-09-14 15:20:26 -0300

    Adjust in DER and drop tables file

--- db/drop_tables.sql
+++ db/drop_tables.sql
@@ -1,6 +1,12 @@
 -- Drop all tables of Poty Games Database
 DROP TABLE flyway_schema_history;
 
+DROP TABLE order_addresses;
+DROP TABLE order_items;
+DROP TABLE orders;
+
+DROP TABLE shopping_cart_items;
+
 DROP TABLE inventory_items;
 DROP TABLE products;
 DROP TABLE product_types;
